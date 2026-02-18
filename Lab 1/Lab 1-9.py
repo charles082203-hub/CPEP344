ef7cf2c8f38d5375e321b06645dbeefda5afd6df
charles082203-hub <charles082203@gmail.com>
2026-02-18 21:22:26 +0800

    Create Lab 1-9.py

---
+++ Lab 1/Lab 1-9.py
@@ -0,0 +1,9 @@
+a = float(input("Enter a value for a: "))
+b = float(input("Enter a value for b: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("\nThat's all, folks!")
